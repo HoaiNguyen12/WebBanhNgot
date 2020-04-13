ca3e14a32af53a3980696d6af1343b3af480e122
HoaiNguyen1128 <Hoainguyen.1128@gmail.com>
2020-04-13 22:58:55 +0700

    hiển thị sp

--- WebCakeCLI/ClientApp/src/components/Product/Product.js
+++ WebCakeCLI/ClientApp/src/components/Product/Product.js
@@ -4,17 +4,21 @@ class Product extends Component {
 	render() {
 		return (
 			<div className="col-lg-4 col-md-6">
-				<div className="gallery-single fix">
-					<img src={"images/" + this.props.product.productImage} className="img-fluid" style={{ height : 20 +'em', width: 25 + 'em'}} />
-					<div>
-						<h4>{this.props.product.productName}</h4>
-							<p>{this.props.product.productDescription}</p>
-							<p className="Price"> {this.props.product.productPrice} VNĐ</p>
-						</div>
+				<div id="product">
+					<div className="box button-group filter-button-group">
+						<img src={"images/" + this.props.product.productImage} className="thumbnail" />
+						<i className="fa fa-plus"  />
+						<h2>{this.props.product.productName}</h2>
+						<p>$ { this.props.product.productPrice }</p>
 				</div>
 			</div>
+
+		
+			</div >
 		)
 	}
+
+	
 }
 
 export default Product;
