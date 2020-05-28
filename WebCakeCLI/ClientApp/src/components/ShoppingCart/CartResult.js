05a188d1d5640642b7248e5ecf522ab8a7ccbfe4
Huong <dohuong2198@gmail.com>
2020-05-28 16:55:26 +0700

    sửa component CartResult

--- WebCakeCLI/ClientApp/src/components/ShoppingCart/CartResult.js
+++ WebCakeCLI/ClientApp/src/components/ShoppingCart/CartResult.js
@@ -39,7 +39,7 @@ class CartResult extends Component {
         var total = 0;
         if (cart.length > 0) {
             for (var i = 0; i < cart.length; i++) {
-                total += cart[i].product.price * cart[i].quantity;
+                total += cart[i].product.productPrice * cart[i].quantity;
             }
         }
         return total;
