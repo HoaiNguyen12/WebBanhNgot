d63bac806847cb673d624fd77c0f9521a73030a2
Huong <dohuong2198@gmail.com>
2020-05-31 09:58:05 +0700

    thêm component

---
+++ WebCakeCLI/ClientApp/src/components/Admin/OrderManagement/OrderForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class categoryForm extends Component {
+    render(){
+        return(
+            <div className="container p-5">
+                <h1 style={{textAlign: 'center'}}>Cập nhật đơn hàng</h1>
+                <form >
+                    <div className="form-group">
+                        <label style={{fontSize:'18px'}}>Trạng thái:  </label>
+                        <select class="form-control" name="sellist1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </div>
+                    <Link className="btn btn-primary mr-2" to="/admin/orderList">Quay Lại</Link>
+                    <button type="button" className="btn btn-primary">Lưu Lại</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default categoryForm;
---
+++ WebCakeCLI/ClientApp/src/components/Admin/OrderManagement/OrderList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import NavabarAdmin from './../NavbarAdmin';
+import LeftAdmin from './../LeftAdmin';
+
+import { Link } from 'react-router-dom';
+class OrderList extends Component {
+    render(){
+        return(
+            <div>
+                <NavabarAdmin />
+               <div className="themed-container" fluid="sm" style={{height: '600px'}}>
+                    <div className="row" style={{height: '600px'}}>
+                        <div className="col-3" style={{backgroundColor:'#d0a772'}}>
+                            <LeftAdmin />
+                        </div>
+                        <div className="col-9" >
+                            <div className="container">
+                                <h1 className="mt-3">Quản lý đơn hàng</h1>
+                                <table className="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>STT</th>
+                                            <th>Mã đơn hàng</th>
+                                            <th>Tên khách hàng</th>
+                                            <th>Ngày đặt</th>
+                                            <th>Hình thức thanh toán</th>
+                                            <th>Ghi chú</th>
+                                            <th>Trạng thái</th>
+                                            <th>Sửa</th>
+                                            <th>Xóa</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td><Link to="#"><i className="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></Link></td>
+                                            <td><i className="fa fa-trash-o fa-2x" aria-hidden="true"></i></td>
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default OrderList;
